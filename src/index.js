e7d4ae0f0805f855b7b2e32c9f79511761005ff6
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-04-08 18:33:17 -0700

    add task builder

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { tasksBuilder } from './tasks';
 const projectForm = document.getElementById('project-form');
 projectForm.addEventListener('submit', projectsBuilder);
 
-//const taskForm = document.getElementById('task-form');
-//taskForm.addEventListener('submit', tasksBuilder);
+const taskForm = document.getElementById('task-form');
+taskForm.addEventListener('submit', tasksBuilder);
 
 projectsView();
--- src/tasks.js
+++ src/tasks.js
@@ -1,14 +1,21 @@
 const tasks = [];
-const taskForm = document.getElementById('task-form');
 
 const taskFactory = () => {
-    tasks.push({
+    const taskForm = document.getElementById('task-form');
+    const newTask = {
         title: taskForm.title.value,
         description: taskForm.description.value,
         dueDate: taskForm.dueDate.value,
         priority: taskForm.priority.value,
         project: taskForm.project.value,
-    });
-    return { title, description, dueDate, priority, project };
+    };
+    taskForm.reset();
+    return newTask;
 }
 
+const tasksBuilder = (e) => {
+    e.preventDefault();
+    tasks.push(taskFactory());
+}
+
+export { tasksBuilder };
