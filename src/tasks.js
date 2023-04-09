b660bf6c45327236ce6275cd484bf01eb66e227a
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-04-08 19:32:52 -0700

    add all task display

--- src/tasks.js
+++ src/tasks.js
@@ -13,9 +13,28 @@ const taskFactory = () => {
     return newTask;
 }
 
+const tasksDisplay = () => {
+    const taskList = document.querySelector('.tasks');
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.lastChild);
+    }
+    for (const task of tasks) {
+        const taskDiv = document.createElement('div');
+        taskList.appendChild(taskDiv);
+        for (const property in task) {
+            const propertyDiv = document.createElement('div');
+            propertyDiv.classList.add(`${property}`);
+            propertyDiv.textContent = `${task[property]}`;
+            taskDiv.appendChild(propertyDiv);
+        }
+    }
+}
+
 const tasksBuilder = (e) => {
     e.preventDefault();
     tasks.push(taskFactory());
+    console.log(tasks);
+    tasksDisplay();
 }
 
 export { tasksBuilder };
