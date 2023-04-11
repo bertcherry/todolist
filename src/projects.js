5f7f0b6bdad8efd474d8198894cf73cf553d8340
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-04-10 23:20:57 -0700

    add delete projects

--- src/projects.js
+++ src/projects.js
@@ -1,4 +1,6 @@
 import { filterFactory } from "./view";
+import pencilIcon from './pencil.svg';
+import deleteIcon from './delete.svg';
 
 const projects = [{name: 'General', description: 'Default project'}]
 
@@ -24,15 +26,44 @@ const projectsDisplay = (listName, type) => {
             projectItem.textContent = project.name;
         }
         if (type === 'li') {
+            const projectDiv = document.createElement('div');
             const projectLink = document.createElement('a');
             projectLink.setAttribute('href', '');
             projectLink.id = project.name;
             projectLink.textContent = project.name;
-            projectItem.appendChild(projectLink);
+            projectDiv.appendChild(projectLink);
             projectLink.addEventListener('click', filterFactory('project', project.name).filterTasks);
+            if (projects.indexOf(project) != 0) {
+                projectDiv.appendChild(iconFactory('delete', deleteIcon, deleteProject, project));
+            }
+            //projectLink.appendChild(iconFactory('edit', editIcon, editProject));
+            projectItem.appendChild(projectDiv);
         }
         projectList.appendChild(projectItem);
     }
+}
+
+const iconFactory = (type, reference, action, project) => {
+    const btn = document.createElement('button');
+    btn.classList.add(type + '-btn');
+    btn.setAttribute('aria-label', type);
+    btn.id = project.name;
+    const img = document.createElement('img');
+    img.src = reference;
+    img.id = project.name;
+    btn.appendChild(img);
+    btn.addEventListener('click', action);
+    return btn;
+}
+
+const deleteProject = (e) => {
+    const projectId = e.target.id;
+    console.log(projectId);
+    const index = projects.map(i => i.name).indexOf(`${projectId}`);
+    if (index > 0) {
+        projects.splice(index, 1);
+    }
+    projectsView();
 }
 
 const projectsView = () => {
